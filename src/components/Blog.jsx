9f9b2e38f695397f798bad586690d6b0988dd563
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-05-19 17:33:39 +0800

    blog elements

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { ArrowRight, Eye, MessageSquare } from "react-feather";
+import { blogData } from "../constant/BlogData";
 
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <section id="blog" className="max-w-screen-lg px-10 pt-10 pb-8">
+      <div className="blog-container">
+        <h2 className="section-title">
+          Our Blogs Coffee with trending topic for this week
+        </h2>
+
+        <div className="blog-content grids">
+          {blogData.map((blog) => (
+            <article className="blog-card" key={blog.id}>
+              <div className="blog-image">
+                <img src={blog.image} alt={blog.title} className="blog-img" />
+                <a href="#" className="blog-button">
+                  <ArrowRight />
+                </a>
+              </div>
+              <div className="blog-data">
+                <h2 className="blog-title">{blog.title}</h2>
+                <p className="blog-description">{blog.description}</p>
+                <div className="blog-footer">
+                  <MessageSquare />
+                  <span>{blog.comment}</span>
+                </div>
+                <div className="blog-footer">
+                  <Eye />
+                  <span>{blog.like}</span>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Blog;
---
+++ src/constant/BlogData.jsx
@@ -0,0 +1,23 @@
+import Blog1 from "../assets/img/blog1.png";
+import Blog2 from "../assets/img/blog2.png";
+
+export const blogData = [
+  {
+    id: "1",
+    image: Blog1,
+    title: "10 Coffee Recommendations",
+    description:
+      "The blog about coffee will provide recommendations on preparing and enjoying a good quality coffee.",
+    comment: "12",
+    like: "10.6k",
+  },
+  {
+    id: "2",
+    image: Blog2,
+    title: "12 Benefits Of Drinking Coffee",
+    description:
+      "The blogs about coffee will help you a lot about how it is prepared, its waiting time, for a good quality coffee.",
+    comment: "33",
+    like: "12.5k",
+  },
+];
